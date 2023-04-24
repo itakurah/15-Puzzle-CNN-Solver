4d7f657288be70d967d3074365780ee0895e7246
itakurah <122729995+itakurah@users.noreply.github.com>
2023-04-24 11:13:23 +0200

    added custom exception class

---
+++ app/src/main/java/solver/PuzzleException.java
@@ -0,0 +1,14 @@
+package solver;
+
+/**
+ *
+ */
+public class PuzzleException extends RuntimeException {
+    public PuzzleException(String message) {
+        super(message);
+    }
+
+    public PuzzleException() {
+        super("Something went wrong");
+    }
+}
